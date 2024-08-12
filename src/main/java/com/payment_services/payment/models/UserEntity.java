218cbfac9239c6698bd19e93018a4bfbdd5aa22f
Eruviel Garcia <eruvielgs@gmail.com>
2024-08-12 15:27:09 -0600

    se agrego la entidad user

---
+++ src/main/java/com/payment_services/payment/models/UserEntity.java
@@ -0,0 +1,22 @@
+package com.payment_services.payment.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private LocalDateTime createdAt;
+}
